55129eca8b6925fa7f22a95183af256e25236a37
iqrahassan074 <iqrasher142@gmail.com>
2025-02-15 21:15:47 +0500

    make some changes

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,26 @@
+// import { createClient } from 'next-sanity'
+
+
+// export const client = createClient({
+//   projectId: "pbl5pxzd", 
+//   dataset: "production",  
+//   apiVersion:"2023-01-01",
+//   useCdn: true,
+//   token: "skx78wh3FQklNtLtrrwZxoDZcNSszdPnKqfjcB8lLHAskWy4n8xbHdulwEUuJ0lubnubQuGqymsIiiGMXlMBjS29F3DZz27RYPKcbg5d1xSNUOSWCpsNPX4DBjUJ6jBFy1ethdFd1ezIgNttnPIQ0lOFfRpqdvbrA44wcuRYYVnqd5rkWHga",
+// })
+
+
+
+
+
+
 import { createClient } from 'next-sanity'
 
-
 export const client = createClient({
-  projectId: "pbl5pxzd", 
-  dataset: "production",  
-  apiVersion:"2023-01-01",
-  useCdn: true,
-  token: "skx78wh3FQklNtLtrrwZxoDZcNSszdPnKqfjcB8lLHAskWy4n8xbHdulwEUuJ0lubnubQuGqymsIiiGMXlMBjS29F3DZz27RYPKcbg5d1xSNUOSWCpsNPX4DBjUJ6jBFy1ethdFd1ezIgNttnPIQ0lOFfRpqdvbrA44wcuRYYVnqd5rkWHga",
+  projectId: "pbl5pxzd",   
+  dataset: "production",   
+  
+  apiVersion: "2023-01-01",  
+  useCdn: true,             
+  token: "skx78wh3FQklNtLtrrwZxoDZcNSszdPnKqfjcB8lLHAskWy4n8xbHdulwEUuJ0lubnubQuGqymsIiiGMXlMBjS29F3DZz27RYPKcbg5d1xSNUOSWCpsNPX4DBjUJ6jBFy1ethdFd1ezIgNttnPIQ0lOFfRpqdvbrA44wcuRYYVnqd5rkWHga",  
 })
